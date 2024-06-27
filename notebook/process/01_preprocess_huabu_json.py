17543d8e47072fe4ac496281da632a4a91f7c21d
easonsshi <easonsshi@tencent.com>
2024-06-27 20:56:20 +0800

    [feat] huabu 数据处理

---
+++ notebook/process/01_preprocess_huabu_json.py
@@ -0,0 +1,42 @@
+""" 
+预处理所给的xlsx形式的画布数据
+- 去除错误数据
+- 规范文件名
+"""
+
+# %%
+import os, json
+import pandas as pd
+from tqdm import tqdm
+DIR_data = f"../../data/画布json-v240627"
+
+# %%
+df_raw = pd.read_excel(f"{DIR_data}/taskflow-1.xlsx", header=None)
+df_raw.head()
+
+# %%
+# keep only the first 3 columns, named [id, name, json]
+df = df_raw.iloc[:, :3]
+df.columns = ["id", "name", "json"]
+df.head()
+# %%
+json.loads(df.iloc[0, 2])
+# %%
+def f_filter(s):
+    try:
+        res = json.loads(s)
+        return True
+    except:
+        return False
+df_filtered = df[df["json"].apply(f_filter)]
+print(f"#samples: {len(df)}, #filtered: {len(df_filtered)}")
+
+# %%
+DIR_output = f"{DIR_data}/json_step0"
+os.makedirs(DIR_output, exist_ok=True)
+for i, row in tqdm(df_filtered.iterrows()):
+    fn = f"{i:03d}-{str(row['name']).replace('/', '_')[:10]}.json"      # 避免文件名过长, 不合法
+    with open(f"{DIR_output}/{fn}", "w") as f:
+        f.write(json.dumps(json.loads(row["json"]), indent=4, ensure_ascii=False))
+
+# %%
