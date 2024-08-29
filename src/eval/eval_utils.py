bf40d9ee192866c0117e1f3e6444c98f1a199534
easonsshi <easonsshi@tencent.com>
2024-08-29 20:19:10 +0800

    [fix] evaluation errors!

--- src/eval/eval_utils.py
+++ src/eval/eval_utils.py
@@ -8,7 +8,6 @@ from engine import _DIRECTORY_MANAGER, LLM_CFG, init_client, PDL, Config, UserPr
 from simulator import SimulatorV2
 from utils.jinja_templates import jinja_render
 
-
 # 转为便于表格展示的两个形式的数据
 # 对于每一组对话, 第一行为meta信息, 第二行为bot  greeting, 后面的 U/B 之间的会话
 def parse_conv(conversation_s:str):
@@ -17,27 +16,23 @@ def parse_conv(conversation_s:str):
     for line in conversation_s.split("\n"):
         if not line.strip(): continue
         if line.startswith("[BOT]"):
-            if role == "user":
-                convs.append(utterence)
+            if role!='bot':
+                if utterence: convs.append(utterence)
                 utterence = line
-            else:
-                if utterence: utterence += "\n"
-                utterence += line
+            else: utterence += f"\n{line}"
             role = "bot"
         elif line.startswith("[USER]"):
-            if role == "bot": 
-                convs.append(utterence)
+            if role!='user': 
+                if utterence: convs.append(utterence)
                 utterence = line
-            else:
-                if utterence: utterence += "\n"
-                utterence += line
+            else: utterence += f"\n{line}"
             role = "user"
         else:
-            if utterence: utterence += "\n"
-            utterence += line
+            if utterence: utterence += f"\n{line}"
     convs.append(utterence)
+    if len(convs) % 2 != 1: # Make sure that the bot finish the last user query
+        print(f"WARNING! conversation not end with bot! \n{convs}")
     return convs
-
 
 # 转为规范的数据格式, {workflow_name, workflow_id, user_profile, simulated_conversation}
 def convert_conv(df:pd.DataFrame):
@@ -49,9 +44,8 @@ def convert_conv(df:pd.DataFrame):
     for s,e in itertools.pairwise(idx):
         id_, user_profile = df.iloc[s,0], df.iloc[s,1]
         workflow_name, workflow_id = id_.split("_", 1)
-        # print(workflow_name, workflow_id)
         conv = df.iloc[s+1:e, :].values.tolist()
-        # print(conv)
+        assert len(conv) % 2 == 1, f"Invalid conversation: {conv}"
         conversations.append({
             "workflow_name": workflow_name,
             "workflow_id": int(workflow_id),
@@ -63,7 +57,7 @@ def convert_conv(df:pd.DataFrame):
 
 def task_simulate(cfg: Config, user_profile_json: Dict, workflow_name, ofn) -> None:
     """ One simulation task
-    TODO: add retry & raise!
+    [x] add retry & raise? -- DONE in `Evaluator.run_simulations()`
     """
     fp = open(ofn, "a+", encoding="utf-8")
     user_profile = UserProfile.load_from_dict(user_profile_json)
@@ -80,9 +74,10 @@ def task_simulate(cfg: Config, user_profile_json: Dict, workflow_name, ofn) -> N
     fp.flush()
     fp.close()
 
-def valid_judge_result(jr: Dict):
+def valid_judge_result(jr: Dict, num_rounds: int):
     assert "overall" in jr and "detailed" in jr, f"Invalid judge result: {jr}"
     assert "score" in jr["overall"] and isinstance(jr["overall"]["score"], (int, float)), f"Invalid judge result: {jr}"
+    assert len(jr["detailed"]) == num_rounds, f">> {len(jr['detailed'])}!={num_rounds}\nInvalid judge result: {jr}"
     for _, round_eval in jr["detailed"].items():
         assert "score" in round_eval and isinstance(round_eval["score"], (int, float)), f"Invalid judge result: {jr}"
         errors = round_eval["errors"]
@@ -92,8 +87,10 @@ def valid_judge_result(jr: Dict):
 
 def task_judge(conv, ofn, client: OpenAIClient) -> None:
     workflow_name = conv["workflow_name"]
+    num_rounds = len(conv["simulated_conversation"]) // 2
     pdl = PDL.load_from_file(_DIRECTORY_MANAGER.DIR_huabu_step3 / f"{workflow_name}.yaml")
-    s_conv = tabulate.tabulate(conv["simulated_conversation"], headers="keys", showindex=False, maxcolwidths=100) # , tablefmt='psql'
+    s_conv = tabulate.tabulate(conv["simulated_conversation"], headers="keys", showindex=False, maxcolwidths=100, tablefmt='psql')
+    
     prompt = jinja_render(
         "scorer_detailed.jinja",
         conversation=s_conv,
@@ -105,8 +102,8 @@ def task_judge(conv, ofn, client: OpenAIClient) -> None:
             jr = Formater.parse_llm_output_yaml(res)
             if "error" in jr:
                 continue
-            # TODO: check the format of res
-            valid_judge_result(jr)
+            # DONE @240828: check the format of res
+            valid_judge_result(jr, num_rounds)
             out = {
                 "workflow_name": workflow_name,
                 "workflow_id": conv["workflow_id"],
@@ -121,39 +118,10 @@ def task_judge(conv, ofn, client: OpenAIClient) -> None:
                 f.write(json.dumps(out, ensure_ascii=False) + "\n")
             break
         except Exception as e:
+            traceback.print_exc()
             continue
     else:
-        traceback.print_exc()
+        print(f"ERROR!!! conv:\n{conv}")
+        print(f"prompt: {prompt}")
         raise Exception(f"Judge failed 3 times for {workflow_name}")
 
-# %%
-from easonsi import utils
-import traceback
-def remove_dup_lines(fn):
-    # for judge_result
-    data = utils.LoadJsonl(fn)
-    data_filtered = []
-    exist = set()
-    for d in data:
-        key = (d['workflow_name'], d["workflow_id"])
-        if key not in exist:
-            data_filtered.append(d)
-            exist.add(key)
-    utils.SaveJsonl(data_filtered, fn)
-# remove_dup_lines("/work/huabu/data/v240820/simulated/v240828/eval_gpt_detailed.jsonl")
-def remove_unvalid_judge_result(fn):
-    data = utils.LoadJsonl(fn)
-    data_filtered = []
-    cnt_removed = 0
-    for d in data:
-        try:
-            valid_judge_result(d["judge_result"])
-            data_filtered.append(d)
-        except:
-            print(f"Invalid judge result: {d['workflow_name']}, {d['workflow_id']}")
-            traceback.print_exc()
-            cnt_removed += 1
-    if cnt_removed < 5:
-        utils.SaveJsonl(data_filtered, fn)
-# remove_unvalid_judge_result("/work/huabu/data/v240820/simulated/v240828_02/eval_gpt_detailed.jsonl")
-# %%
--- src/eval/evaluator.py
+++ src/eval/evaluator.py
@@ -4,32 +4,31 @@
 2. 整合结果, 格式化输出;
 3. 集中化评估, 格式化输出;
 
-- [ ] 保存CoT信息 (相关meta)
-- [ ] 增加偏题的机制 (30% 的概率) -- 在persona中
-- [ ] 优化 print
-- [ ] prompt 优化: 尝试对于API和ANSWER节点分类考虑
-- [ ] 优化 EL 部分的系统提示
-- [x] 自己部署一下 LLM
-- [ ] 实现LLM评分, 比较一致性
+- [ ] 保存CoT信息 (相关meta)    #P2
+- [x] 增加偏题的机制 (30% 的概率) -- 在persona中
+- 优化输出
+    - [x] 整个 evaluation 过程中的 print
+    - [ ] 简化 simulateion 过程中的输出
+    - [ ] 优化 EL 部分的系统提示
+- [x] prompt 优化: 尝试对于API和ANSWER节点分类考虑
+    有待评估效果
+- [x] 实现LLM评分, 比较一致性
 - [ ] 保存 meta 信息 #P1
+- [ ] BUG: LLM evaluation 可视化结果和turns没有对上 #P1
 
 output: 
     https://doc.weixin.qq.com/sheet/e3_Aa8AFwbhAEsNvUX5XUlTXSTGWsT5A?scode=AJEAIQdfAAok83fN9fAcMATAZtAPI&tab=d88els
 """
 
 import os, json, tqdm, itertools, pickle, collections, traceback, datetime, argparse, tabulate
-from typing import List, Dict, Optional, Tuple
+from typing import List, Dict, Optional, Tuple, Union
 import pandas as pd
 import concurrent.futures
 from easonsi import utils
-from easonsi.llm.openai_client import OpenAIClient, Formater
-from engine import _DIRECTORY_MANAGER, LLM_CFG, init_client, PDL, Config, UserProfile
-from simulator import SimulatorV2
-from utils.jinja_templates import jinja_render
+from engine import _DIRECTORY_MANAGER, LLM_CFG, init_client, PDL, Config
 from .eval_utils import task_simulate, task_judge, parse_conv, convert_conv
 from.analyzer import Analyzer
-from easonsi.files.gsheet import GSheet
-gsheet = GSheet()
+
 
 
 class Evaluator:
@@ -46,19 +45,39 @@ class Evaluator:
         self.cfg = cfg
         self.version = cfg.simulate_version
         self.output_dir = _DIRECTORY_MANAGER.DIR_simulated_base / cfg.simulate_version
-        self.fn_conversations_for_labeling = self.output_dir / "simulated_conversations.csv"
+        self.fn_conversations_for_excel = self.output_dir / "simulated_conversations.csv"
         self.fn_conversations = self.output_dir / "simulated_conversations.jsonl"
         self.fn_llmscored_raw = self.output_dir / "eval_gpt_detailed.jsonl"
         self.fn_llmscored = self.output_dir / "eval_gpt.jsonl"      # simplified
+        if cfg.to_gsheet: 
+            from easonsi.files.gsheet import GSheet
+            self.gsheet = GSheet()
         
     def main(self):
         # 
+        self.print_header_info(step_name="STEP 1.1: Simulating", infos={k:v for k,v in self.cfg.to_dict().items() if k.startswith("simulate")})
         self.run_simulations()
+        self.print_header_info(step_name="STEP 1.2: Collecting simulated results")
         self.collect_simulated_results()    # If error, check step one!
 
+        self.print_header_info(step_name="STEP 2.1: Evaluating", infos={k:v for k,v in self.cfg.to_dict().items() if k.startswith("judge")})
         self.run_evaluations()
+        self.print_header_info(step_name="STEP 2.2: Collecting evaluation results")
         self.collect_evaluation_results()
 
+    def print_header_info(self, step_name: str, infos: Union[None, Dict, pd.DataFrame]=None):
+        s_print = step_name.center(150, "=") + "\n"
+        if infos is not None:
+            if isinstance(infos, dict):
+                infos = pd.DataFrame([infos]).T
+            elif isinstance(infos, pd.DataFrame):
+                pass
+            else:
+                raise NotImplementedError
+            s_infos = tabulate.tabulate(infos, tablefmt='psql')    # , headers='keys', 
+            s_print += f"--- infos ---\n{s_infos}\n"
+        s_print += "-"*150
+        print(s_print)
 
     def run_simulations(self):
         # config: simulate_*
@@ -158,42 +177,38 @@ class Evaluator:
 
         # 2) convert to U/I utterance format (two columns)
         # 对于每一组对话, 第一行为meta信息, 第二行为bot  greeting, 后面的 U/B 之间的会话
-        if not os.path.exists(self.fn_conversations_for_labeling):
+        if not os.path.exists(self.fn_conversations_for_excel):
             parsed_data = []
             for fn in fns:
                 data = utils.LoadJsonl(self.output_dir / fn)
                 print(f"{fn}: {len(data)}")
-                for i,sample_d in enumerate(data):            # select first 10?
+                for i,sample_d in enumerate(data):
                     parsed_data.append(
                         (f'{sample_d["workflow_name"]}_{i:03d}', sample_d["user_profile"])
                     )
-                    conv = parse_conv(sample_d["simulated_conversation"])
+                    conv = parse_conv(sample_d["simulated_conversation"])  # split conversation str into `utterance` of user/bot
                     parsed_data.append(("START", conv[0]))
                     for j,ssample_str in enumerate(conv[1:]):
                         role = "USER" if j % 2 == 0 else "BOT"
                         parsed_data.append((f"{role}{j//2+1}", ssample_str))
+                    # check len(simulated_conversation) is odd? DONE in `parse_conv`
 
-            df = pd.DataFrame(parsed_data)
-            df.to_csv(self.fn_conversations_for_labeling, index=False)
+            df = pd.DataFrame(parsed_data, columns=["round", "content"])
+            df.to_csv(self.fn_conversations_for_excel, index=False)
             if self.cfg.to_gsheet:
-                gsheet.to_gsheet(df, sheet_name=f"{self.version}_sim")
+                self.gsheet.to_gsheet(df, sheet_name=f"{self.version}_sim")
         else:
-            df = pd.read_csv(self.fn_conversations_for_labeling)
+            df = pd.read_csv(self.fn_conversations_for_excel)
     
         # 3) convert to standard format
         # 转为规范的数据格式, {workflow_name, workflow_id, user_profile, simulated_conversation}
-        if not os.path.exists(self.fn_conversations):
-            conversations = convert_conv(df)
-            utils.SaveJsonl(conversations, self.fn_conversations)
-        else:
-            conversations = utils.LoadJsonl(self.fn_conversations)
+        conversations = convert_conv(df)
+        utils.SaveJsonl(conversations, self.fn_conversations)
     
         ssample_str = tabulate.tabulate(
-            conversations[0]["simulated_conversation"], 
-            headers="keys", showindex=False, maxcolwidths=150) # , tablefmt='psql'
-        print(f"sample conversation from {conversations[0]['workflow_name']}:".center(200, "="))
-        print(ssample_str)    # ["round", "content"]
-        print("=" * 150)
+            conversations[0]["simulated_conversation"],  headers=["round", "content"],
+            showindex=False, maxcolwidths=150, tablefmt="psql") # headers="keys", simple_outline/psql
+        print(f"sample conversation from {conversations[0]['workflow_name']}:\n{ssample_str}")
 
     def run_evaluations(self):
         conversations = utils.LoadJsonl(self.fn_conversations)
@@ -232,28 +247,34 @@ class Evaluator:
         """ 
         FIXED: the eval format checked in `task_judge`
         """
-        df = pd.read_json(self.fn_llmscored, lines=True)
+        df_labelled_raw = pd.read_json(self.fn_llmscored, lines=True)
         # 1) convert to format of doc
         #   cols: [error_types, score, misc]
         converted = []
-        for idx, row in df.iterrows():
+        for idx, row in df_labelled_raw.iterrows():
             id_ = f"{row['workflow_name']}_{row['workflow_id']:03d}"
             jr = row["judge_result"]
             converted.append((None, jr["overall"]["score"], id_))
             converted.append((None, None, None))
-            for round_id, round_eval in sorted(jr["detailed"].items(), key=lambda x: x[0]):
+            for round_id, round_eval in sorted(jr["detailed"].items(), key=lambda x: int(x[0].split("-")[-1])):
                 errors = round_eval["errors"]
                 error_types_str = ",".join(errors.keys() if errors else [])
                 converted.append((None, None, round_id))
                 converted.append((error_types_str, round_eval["score"], str(errors)))
-        df_converted = pd.DataFrame(converted, columns=["misc", "score", "error_types"])
+        df_labelled_for_excel = pd.DataFrame(converted, columns=["error_types", "score", "misc"])
+        # DONE: concate with formated conversations in `self.fn_conversations_for_labeling`
+        df_conversation_for_excel = pd.read_csv(self.fn_conversations_for_excel)
+        assert len(df_conversation_for_excel) == len(df_labelled_for_excel)
+        _df_out = pd.concat([df_conversation_for_excel, df_labelled_for_excel], axis=1)
         if self.cfg.to_gsheet:  # you can also save to csv
-            gsheet.to_gsheet(df_converted, sheet_name=f"{self.version}_eval")
+            self.gsheet.to_gsheet(_df_out, sheet_name=f"{self.version}_eval")
         
         # 2) analyze
-        analyzer = Analyzer(df, output_dir=self.output_dir)
+        analyzer = Analyzer(df_labelled_raw, output_dir=self.output_dir)
         _ = analyzer.stat_num_rounds()
         _ = analyzer.stat_scores_overall()
         _ = analyzer.stat_error_types()
         grouped_passrate = analyzer.stat_grouped_passrate()
-        print(grouped_passrate)
+        # useful to copy and paste into Excel
+        tabbed_str = "\t".join(grouped_passrate.columns) + "\n" + "\n".join(["\t".join(map(str, row)) for row in grouped_passrate.values])
+        print(tabbed_str)
