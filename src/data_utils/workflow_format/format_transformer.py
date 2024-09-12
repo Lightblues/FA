7db9634ad93f79c284886c6ec44c3c6f785eea93
siqiicai <siqiicai@tencent.com>
2024-09-12 12:18:24 +0800

    templates and codes for transforming the format of APIs and workflows from public datasets.

---
+++ src/data_utils/workflow_format/format_transformer.py
@@ -0,0 +1,65 @@
+import re
+import os
+import time
+from tqdm import tqdm
+import json
+import pandas as pd
+import datetime
+import argparse
+from typing import Optional, List
+from tabulate import tabulate
+
+from utils.jinja_templates import jinja_render
+from easonsi.llm.openai_client import OpenAIClient, Formater
+from engine import init_client, LLM_CFG
+
+# class for transforming workflow format
+class FormatTransformer:
+    llm: OpenAIClient = None
+    template_file: str = None
+    return_codeblock_format: str = None   # python, PDL, mermaid, ...
+    
+    def __init__(self, model_name, template_file, return_format) -> None:
+        self.llm = init_client(llm_cfg=LLM_CFG[model_name])
+        self.template_file = template_file
+        self.return_codeblock_format = return_format
+    
+    def transform(self, NL_desc: dict, apis: list) -> str:
+        NL_desc['available_apis'] = apis
+        
+        prompt = jinja_render(
+            self.template_file,
+            NL=json.dumps(NL_desc, indent=2)
+        )
+        # print('=' * 20 + 'prompt ' + '=' * 20)
+        # print(prompt)
+        llm_resp = self.llm.query_one(prompt)
+        # print('=' * 20 + ' resp ' + '=' * 20)
+        # print(llm_resp)
+        
+        workflow = Formater.parse_codeblock(llm_resp, self.return_codeblock_format)
+        return workflow
+
+
+# class for transforming API format
+class APITransformer:
+    llm: OpenAIClient = None
+    template_file: str = None
+    
+    def __init__(self, model_name, template_file, return_format) -> None:
+        self.llm = init_client(llm_cfg=LLM_CFG[model_name])
+        self.template_file = template_file
+    
+    def transform(self, api: dict) -> str:
+        prompt = jinja_render(
+            self.template_file,
+            API=json.dumps(api, indent=2)
+        )
+        # print('=' * 20 + 'prompt ' + '=' * 20)
+        # print(prompt)
+        llm_resp = self.llm.query_one(prompt)
+        # print('=' * 20 + ' resp ' + '=' * 20)
+        # print(llm_resp)
+        
+        transformed_api = Formater.parse_llm_output_json(llm_resp)
+        return transformed_api
